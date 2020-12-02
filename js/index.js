78fcba74135d568993f5b5ed4275232306ac15d6
Marc Font <marcfontcaballe@gmail.com>
2020-12-03 00:32:49 +0100

    javascript game

--- js/index.js
+++ js/index.js
@@ -2,26 +2,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-function sayHello() {
-    document.getElementById("demo").innerHTML = "Congratulations! You are Pollini!";
-    window.alert("Welcome to a whole new world of knowledge and dreams!");
-    // document.getElementById("bodyid").style.backgroundColor="gold";
-    // document.getElementById("wrist").innerHTML = "Canello";
-    // document.querySelector(".footer").style.backgroundColor="gold";
-    // document.getElementsByClassName('card')[2].style.backgroundColor="red";
-    // document.querySelector(".card-container").children[0].innerHTML = "hola";
-
-  }
-  //Ojo que amb doble entrecomillado no em funcionava el selector
-  function changeOrder(){
-    const firstCard = document.querySelector('.card-container').children[0].innerHTML;
-    document.querySelector('.card-container').children[0].innerHTML = document.querySelector('.card-container').children[1].innerHTML;
-    document.querySelector('.card-container').children[1].innerHTML = document.querySelector('.card-container').children[2].innerHTML;
-    document.querySelector('.card-container').children[2].innerHTML = document.querySelector('.card-container').children[3].innerHTML;
-    document.querySelector('.card-container').children[3].innerHTML = firstCard;
-  }
-
-
   //jQuery example (if you want to try to make your own you can start with more simple ones)
 
   $('.container h1').on('click', function(){
@@ -30,7 +10,7 @@ function sayHello() {
     //generate a random number between 0 and 100
     const randomSize = Math.floor(Math.random() * 100);
 
-    //generate two random HEX colors
+    //generate four random HEX colors
     const randomHex1 = '#' + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10);
     const randomHex2 = '#' + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10);
     const randomHex3 = '#' + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10);
@@ -51,4 +31,42 @@ function sayHello() {
     $('.my_navbar').css('background-color', randomHex4);
   });
 
+$('h3').on('click', function(){
+console.log('Funciona?');
+
+// $(this).css('background-color', 'green');
+$(this).css('display', 'none');
+
+
 });
+
+$('a img').on('click', function(event){
+      event.preventDefault();
+      console.log('hola?');
+    $(this).css('display', 'none');
+  });
+
+
+
+});
+
+function sayHello() {
+    document.getElementById("demo").innerHTML = "Congratulations! You are Pollini!";
+    window.alert("Welcome to a whole new world of knowledge and dreams!");
+    // document.getElementById("bodyid").style.backgroundColor="gold";
+    // document.getElementById("wrist").innerHTML = "Canello";
+    // document.querySelector(".footer").style.backgroundColor="gold";
+    // document.getElementsByClassName('card')[2].style.backgroundColor="red";
+    // document.querySelector(".card-container").children[0].innerHTML = "hola";
+
+  }
+
+  //Ojo que amb doble entrecomillado no em funcionava el selector
+  function changeOrder(){
+    const firstCard = document.querySelector('.card-container').children[0].innerHTML;
+    document.querySelector('.card-container').children[0].innerHTML = document.querySelector('.card-container').children[1].innerHTML;
+    document.querySelector('.card-container').children[1].innerHTML = document.querySelector('.card-container').children[2].innerHTML;
+    document.querySelector('.card-container').children[2].innerHTML = document.querySelector('.card-container').children[3].innerHTML;
+    document.querySelector('.card-container').children[3].innerHTML = firstCard;
+  }
+
