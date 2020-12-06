35c0f92f974114d19c368998ae647289bcffcfad
Marc Font <marcfontcaballe@gmail.com>
2020-12-06 20:51:59 +0100

    table

--- js/technique.js
+++ js/technique.js
@@ -2,8 +2,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-  //jQuery example (if you want to try to make your own you can start with more simple ones)
-
 
 
 $('.card-technique1').on('click', function(event){
