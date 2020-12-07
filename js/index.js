972171874e67b6d82dfc6a292a0967cc17825072
Marc Font <marcfontcaballe@gmail.com>
2020-12-07 21:31:09 +0100

    burger

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+  console.log("ready!");
 
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+
+if(!menuOpen) {
+  menuBtn.classList.add('open');
+  menuOpen = true;
+} else {
+menuBtn.classList.remove('open');
+menuOpen = false;
+}
+
+});
+
+
+});
